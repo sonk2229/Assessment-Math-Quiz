a934f6d394a5cfb6285a058b02aa53a342bf90f9
sonk2229 <159740652+sonk2229@users.noreply.github.com>
2024-06-07 14:46:38 +1200

    Add files via upload
    I got it to say you got it correct if you answer it right now i need to say if you get it incorrect

--- Assessment_2A_Question.py
+++ Assessment_2A_Question.py
@@ -1,9 +1,10 @@
 # checks for an integer more than 0 (allows <enter>)
+
 def int_check(question):
 
     while True:
 
-        error = "Please enter an integer that is 1 or more."
+        error = "Please enter an integer that is a number ."
 
         to_check = input(question)
 
@@ -36,6 +37,70 @@ print("Math Quiz")
 print()
 
 # Instructions
+# Check that users have entered a valid
+# option based on a list
+
+
+def string_checker(question, valid_ans=("yes", "no")):
+    error = f"Please enter a valid option from the following list: {valid_ans}"
+
+    while True:
+
+        # Get user response and amke sure it's lowercase
+        user_response = input(question).lower()
+
+        for item in valid_ans:
+            # check if the user response is a word in the list
+            if item == user_response:
+                return item
+
+            # check if the user response is the same as
+            # the first letter of an itme in the list
+            elif user_response == item[0]:
+                return item
+
+        # print error if user does not enter something that is valid
+        print(error)
+        print()
+
+
+def instruction():
+    print('''
+
+    **** Instructions ****
+
+To begin, choose the number of Questions you would like to do
+and what type of difficulty the Quiz in on for the Questions there are easy, normal
+and then hard. 
+
+
+Then choose how many Questions you'd like to do <enter> 
+for infinite mode.
+
+Your goal is to try to get all the Question's correct.
+
+Press <xxx> to end game at anytime 
+
+Good Luck!
+    ''')
+
+
+# Main routine
+print()
+print("Math Quiz")
+print()
+
+# loop for testing purposes
+
+# ask user if they want to see the instructions and display
+# them if requested
+want_instructions = string_checker("Do you want to read the instruction?")
+
+# checks users enter yes (y) or no (n)
+if want_instructions == "yes":
+    instruction()
+
+print("program continues")
 
 # Ask user for numbers of rounds / infinite mode
 num_Questions = int_check("How many Questions would you like to do? Push <enter> for infinite mode: ")
@@ -77,12 +142,15 @@ while Questions_played < num_Questions:
     print(f" {random_1} + {random_2} ")
 
     # get user choice
-    user_choice = input("Answer: ")
+    user_choice = int_check("")
 
     print(f" {random_1} + {random_2} = {random_1 + random_2} ")
 
-    user_choice = ""
-    if user_choice : (f"{ random_1 + random_2} ")
+    if user_choice == random_1 + random_2:
+        print("you got this correct")
+
+    elif user_choice == random_1 + random_2:
+        print("you got this incorrect ")
 
     # If user choice os the exit code, break the loop
     if user_choice == "xxx":
