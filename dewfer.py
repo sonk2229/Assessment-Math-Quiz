640da44e055dbe103cef248ebdc20b638e874182
sonk2229 <159740652+sonk2229@users.noreply.github.com>
2024-06-20 10:59:57 +1200

    completed assessment

--- dewfer.py
+++ dewfer.py
@@ -4,12 +4,9 @@ def int_check(question):
 
     while True:
 
-        error = "Please enter an integer that is a number ."
+        error = "Please enter an integer that is 1 or more."
 
         to_check = input(question)
-
-        if to_check == "xxx":
-            return "xxx"
 
         # Check for infinite mode
         if to_check == "":
@@ -19,6 +16,7 @@ def int_check(question):
             response = int(to_check)
 
         # checks that the number is more than / equal to 1
+
             if response < 1:
                 print(error)
             else:
@@ -33,10 +31,8 @@ def int_check(question):
 # Intialise game variables
 mode = "regular"
 Questions_played = 0
-Questions_right = 0
-Questions_wrong = 0
 
-quiz_history = []
+
 print("Math Quiz")
 print()
 
@@ -50,7 +46,7 @@ def string_checker(question, valid_ans=("yes", "no")):
 
     while True:
 
-        # Get user response and make sure it's lowercase
+        # Get user response and amke sure it's lowercase
         user_response = input(question).lower()
 
         for item in valid_ans:
@@ -73,7 +69,10 @@ def instruction():
 
     **** Instructions ****
 
-To begin, choose the number of Questions you would like to do.
+To begin, choose the number of Questions you would like to do
+and what type of difficulty the Quiz in on for the Questions there are easy, normal
+and then hard. 
+
 
 Then choose how many Questions you'd like to do <enter> 
 for infinite mode.
@@ -90,6 +89,32 @@ Good Luck!
 print()
 print("Math Quiz")
 print()
+
+# checks for an integer more than 0 (allows <enter>)
+def int_check(question):
+
+    while True:
+
+        error = "Please enter an integer that is 1 or more."
+
+        to_check = input(question)
+
+        # Check for infinite mode
+        if to_check == "":
+            return "infinite"
+
+        try:
+            response = int(to_check)
+
+        # checks that the number is more than / equal to 1
+
+            if response < 1:
+                print(error)
+            else:
+                return response
+
+        except ValueError:
+            print(error)
 
 # loop for testing purposes
 
@@ -128,43 +153,31 @@ while Questions_played < num_Questions:
     # to simulate a question
 
 
-    def gen_number():
-        gen_result = random.randint(1, 10)
-        return gen_result
+    def roll_random():
+        roll_result = random.randint(1, 10)
+        return roll_result
 
 
     # rolls two random numbers and returns total
 
     # roll two random
-    random_1 = gen_number()
-    random_2 = gen_number()
+    random_1 = roll_random()
+    random_2 = roll_random()
 
     # Then + the ramdom_1 & random_2 then give random_1 + random_2 after user gives response
     print(f" {random_1} + {random_2} ")
 
     # get user choice
-    user_choice = int_check("")
+    user_choice = input(" ")
+
+    if user_choice == random_1 + random_2:
+        print("you got this correct")
 
     print(f" {random_1} + {random_2} = {random_1 + random_2} ")
 
     # If user choice os the exit code, break the loop
     if user_choice == "xxx":
         break
-    if user_choice == random_1 + random_2:
-        print("you got this correct")
-    else:
-        print("you got this incorrect ")
-    result = user_choice
-    if result == user_choice == random_1 + random_2:
-        Questions_right += 1
-    else:
-        Questions_wrong += 1
-
-    Question_feedback = f"{Questions_played} "
-    history_item = f"Question: {Questions_played + 1} = {user_choice}"
-
-    print(Question_feedback)
-    quiz_history.append(history_item)
 
     Questions_played += 1
 
@@ -173,26 +186,6 @@ while Questions_played < num_Questions:
         num_Questions += 1
 
 
-# Quiz loop ends here
+# Game loop ends here
 
-# Quiz History / Statistics area
-
-if Questions_played > 0:
-    # calculate Statistics
-    Questions_right = Questions_played - Questions_wrong
-    percent_right = Questions_right / Questions_played * 100
-    percent_wrong = Questions_wrong / Questions_played * 100
-
-    # Output Game Statistics
-    print("Game Statistics")
-    print(f" Won: {percent_right:.2f} \t"
-          f" Lost: {percent_wrong:.2f} \t")
-
-# ask user if they want to see their game history and output it if requested.
-see_history = string_checker("\nDo you want to see your quiz history? ")
-if see_history == "yes":
-    for item in quiz_history:
-        print(item)
-
-print()
-print("Thanks for playing. ")
+# Game History / Statistics area
